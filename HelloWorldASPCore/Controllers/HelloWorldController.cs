afe5df22fb6cca81df9bee3579a3653b65518c07
survival_suit <messier83wait@gmail.com>
2019-05-06 17:16:39 +0400

    New sln is created with default controller *.cs file where changed view method.

---
+++ HelloWorldASPCore/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldASPCore.Controllers
+{
+    public class HelloWorldASPCore : ControllerBase
+    {
+        protected internal string Hello()
+        {
+            return "Hello World!!!";
+        }
+    }
+}
