5a69c73df380dd128d4221f3b1c0646274f472c4
survival_suit <messier83wait@gmail.com>
2019-05-07 17:38:13 +0400

    startup and program was reverted/

--- HelloWorldASPCore/Controllers/HelloWorldController.cs
+++ HelloWorldASPCore/Controllers/HelloWorldController.cs
@@ -3,14 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog.Web;
 
 namespace HelloWorldASPCore.Controllers
 {
     public class HelloWorldASPCore : ControllerBase
     {
+        
         protected internal string Hello()
-        {
+        {   
             return "Hello World!!!";
+                        
         }
     }
 }
--- HelloWorldASPCore/Controllers/ValuesController.cs
+++ HelloWorldASPCore/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NLog;
 
 
 namespace HelloWorldASPCore.Controllers
@@ -10,13 +11,13 @@ namespace HelloWorldASPCore.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
-    {
+    {        
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             //return new string[] { "value1", "value2" };
-            return new string[] { "Hello World!" };
+            return new string[] { "Hello World!" };            
 
         }
 
