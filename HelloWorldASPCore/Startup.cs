1b393256b64256653421e6bbb1c3fab1f3a57efb
CoderGosha <coder.gosha@gmail.com>
2019-05-08 12:19:01 +0300

    Отключил SSL. Добавил NLog.config. Сделал сваггер по умолчанию

--- HelloWorldASPCore/Startup.cs
+++ HelloWorldASPCore/Startup.cs
@@ -52,7 +52,7 @@ namespace HelloWorldASPCore
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             //app.UseHttpsRedirection();
