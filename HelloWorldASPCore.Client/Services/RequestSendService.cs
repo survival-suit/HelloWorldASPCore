a08c12bf7da12e9148c52929cefaf08fad8f4658
survival_suit <messier83wait@gmail.com>
2019-05-17 11:22:33 +0400

    Переделал метод из асинхронного в синхронный.

--- HelloWorldASPCore.Client/Services/RequestSendService.cs
+++ HelloWorldASPCore.Client/Services/RequestSendService.cs
@@ -3,42 +3,47 @@ using System.Text;
 using System.Net;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Specialized;
 
 namespace HelloWorldASPCore.Client.Services
 {
     public class RequestSendService
-    {        
-        public static async Task PostRequestAsync()
+    {
+        //public static async Task PostRequestAsync()
+        public static void PostRequestAsync()
         {
-            try
-            {                
-                byte[] byteArray = Encoding.UTF8.GetBytes("{ \"pathString\": \"C:\\\\BOTS\", \"showFolder\": true}");
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:58195/api/FileSystem");                
-                request.Method = "POST";
-                request.ContentType = "application/json-patch+json";
-                request.Accept = "text/plain";
-                request.ContentLength = byteArray.Length;
+            
+                try
+                {                
+                    byte[] byteArray = Encoding.UTF8.GetBytes("{ \"pathString\": \"C:\\\\BOTS\", \"showFolder\": true}");
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:58195/api/FileSystem");                
+                    request.Method = "POST";
+                    request.ContentType = "application/json-patch+json";
+                    request.Accept = "text/plain";
+                    request.ContentLength = byteArray.Length;
 
-                using (Stream dataStream = request.GetRequestStream())
-                {
-                    dataStream.Write(byteArray, 0, byteArray.Length);
-                }
-
-                HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
-                using (Stream stream = response.GetResponseStream())
-                {
-                    using (StreamReader reader = new StreamReader(stream))
+                    using (Stream dataStream = request.GetRequestStream())
                     {
-                        Console.WriteLine(reader.ReadToEnd());
+                        dataStream.Write(byteArray, 0, byteArray.Length);
                     }
+
+                //HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+                HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            Console.WriteLine(reader.ReadToEnd());
+                        }
+                    }
+                    response.Close();
+                    Console.WriteLine("End Request");
                 }
-                response.Close();
-                Console.WriteLine("End Request");
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
     }
 }
