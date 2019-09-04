2a279dee0d3c2ca3eed6bf4c73a43e9356ee0b58
survivalsuit <messier83wait@gmail.com>
2019-09-04 11:45:22 +0300

    Добавил UserModel.cs галочку.

---
+++ HelloWorldASPCore.Common/UserModels/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldASPCore.Common.UserModels
+{
+    class UserModel
+
+    {
+        /// <summary>
+        /// имя
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// фамилия
+        /// </summary>
+        public string UserSecName { get; set; }
+
+        /// <summary>
+        /// емейл
+        /// </summary>
+        public string UserEmail { get; set; }
+
+        /// <summary>
+        /// пароль
+        /// </summary>
+        public string UserPassword { get; set; }
+
+
+    }
+}
