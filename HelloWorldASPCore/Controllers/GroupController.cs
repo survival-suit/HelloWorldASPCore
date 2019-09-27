8d81883ef6e8aee4b4f12b8d3e20e9a18ee2accb
survivalsuit <messier83wait@gmail.com>
2019-09-27 22:13:20 +0300

    GroupController is changed.

--- HelloWorldASPCore/Controllers/GroupController.cs
+++ HelloWorldASPCore/Controllers/GroupController.cs
@@ -4,8 +4,7 @@ using Microsoft.Extensions.Logging;
 using HelloWorldASPCore.Common.Models;
 using HelloWorldASPCore.Common.RequestModels;
 using System.Linq;
-
-
+using System.Collections.Generic;
 
 namespace HelloWorldASPCore.Controllers
 {
@@ -20,7 +19,7 @@ namespace HelloWorldASPCore.Controllers
             _dataBaseMemory = dataBaseMemory;
         }
 
-        [HttpGet]
+        [HttpPost]
         [Route("Group/AddGroup")]
         public GroupModel AddGroup([FromBody] RequestGroupModel grpRqstMdl)
         {
@@ -41,11 +40,33 @@ namespace HelloWorldASPCore.Controllers
         [Route("Group/DeleteGroup")]
         public ActionResult DeleteGroup(Guid groupGuid)
         {
-            var removeGroup = _dataBaseMemory.GroupModelList.Where( x=> x.GroupGuid == groupGuid).FirstOrDefault();
+            var removeGroup = _dataBaseMemory.GroupModelList.Where(x => x.GroupGuid == groupGuid).FirstOrDefault();
             if (removeGroup != null)
                 _dataBaseMemory.GroupModelList.Remove(removeGroup);
             return Ok();
         }
 
+        [HttpGet]
+        [Route("Group/AddUsersToGroup")]
+        public ActionResult AddUsersToGroup (Guid groupGuid, List<Guid> userGuidList)
+        {
+            var writeGroup = _dataBaseMemory.GroupModelList.Where(x => x.GroupGuid == groupGuid).FirstOrDefault();
+            //var currentUser = new UserModel();
+
+            foreach (Guid guid in userGuidList)
+            {
+                //currentUser = _dataBaseMemory.UserModelList.Where(x => x.UserGuid == guid).FirstOrDefault();
+                writeGroup.GroupUsers.Add(_dataBaseMemory.UserModelList.Where(x => x.UserGuid == guid).FirstOrDefault());
+            }
+
+            if (writeGroup != null)
+                _dataBaseMemory.GroupModelList.Remove(writeGroup);
+            return Ok();
+        }
+
+        // GET       
+        [HttpGet]
+        [Route("Group/ShowAllGroups")]
+        public List<GroupModel> ShowAllGroups() { return _dataBaseMemory.GroupModelList; }
     }
 }
